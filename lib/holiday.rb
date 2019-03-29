84c5ade9ef13aec53816ca300ba1819fec1c1cec
Spencer Lindemuth <lindemut@gmail.com>
2019-03-29 16:32:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}: \n"
     holiday.each do |holiday, supplies|
       puts "#{holiday.capitalize}: "
-      
+
     end
   end
 end
