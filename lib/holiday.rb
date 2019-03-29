65f39b0a1c1fb1ae4fa21a3d973b7262303f4fd9
Spencer Lindemuth <lindemut@gmail.com>
2019-03-29 16:19:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season|
     holiday_string += "#{season}: \n"
     season.each do |holiday|
-      holiday_string += "#{holiday}: "
+      holiday_string += "#{holiday[0]}: "
       holiday_string += holiday[1].join(" , ")
     end
   end
